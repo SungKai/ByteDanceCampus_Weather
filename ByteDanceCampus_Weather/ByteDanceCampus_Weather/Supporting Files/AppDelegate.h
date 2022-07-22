2eed9c87f218f84524bafd0a88ffc610fec1ca19
SungKai <2256047981@qq.com>
2022-07-22 21:49:04 +0800

    [init]初始化设置

---
+++ ByteDanceCampus_Weather/ByteDanceCampus_Weather/Supporting Files/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ByteDanceCampus_Weather
+//
+//  Created by 宋开开 on 2022/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ByteDanceCampus_Weather/ByteDanceCampus_Weather/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ByteDanceCampus_Weather
+//
+//  Created by 宋开开 on 2022/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
