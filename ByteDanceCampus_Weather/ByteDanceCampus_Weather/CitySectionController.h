699232a07a88251d5033c5c5146b6d9c5d0d571e
SSR <88606535+HONEY-SSR@users.noreply.github.com>
2022-07-25 22:19:04 +0800

    [feature]UI绘制 01

---
+++ ByteDanceCampus_Weather/ByteDanceCampus_Weather/CitySectionController.h
@@ -0,0 +1,18 @@
+//
+//  CitySectionController.h
+//  ByteDanceCampus_Weather
+//
+//  Created by SSR on 2022/7/25.
+//
+
+#import <IGListKit/IGListKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CitySectionController : IGListSectionController <
+    RisingRouterHandler
+>
+
+@end
+
+NS_ASSUME_NONNULL_END
