02dfe116d29fa9cf77fc131b20a3bc6d4c9eaee6
CUMT-Atom <cnatom@foxmail.com>
2022-08-13 01:45:20 +0800

    [feature]添加了Daily与Hourly的Model与Bean

---
+++ ByteDanceCampus_Weather/ByteDanceCampus_Weather/Model/ForecastDailyModel.m
@@ -0,0 +1,12 @@
+//
+//  ForecastDailyModel.m
+//  ByteDanceCampus_Weather
+//
+//  Created by atom on 2022/8/12.
+//
+
+#import "ForecastDailyModel.h"
+
+@implementation ForecastDailyModel
+
+@end
---
+++ ByteDanceCampus_Weather/ByteDanceCampus_Weather/Model/ForecastHourlyModel.h
@@ -0,0 +1,16 @@
+//
+//  ForecastHourlyModel.h
+//  ByteDanceCampus_Weather
+//
+//  Created by atom on 2022/8/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ForecastHourlyModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
